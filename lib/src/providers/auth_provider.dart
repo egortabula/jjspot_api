994a723b139f00f00f6df75282063c635ea7b2be
egortabula <egor.vnl@gmail.com>
2024-02-15 23:56:45 +0300

    add create account

--- lib/src/providers/auth_provider.dart
+++ lib/src/providers/auth_provider.dart
@@ -9,6 +9,22 @@ class AuthProvider {
 
   final Account _account;
 
+  Future<Either<AppwriteException, CreateAccountResponse>> createAccount(
+      CreateAccountRequest request) async {
+    try {
+      final user = await _account.create(
+        userId: ID.unique(),
+        email: request.email,
+        password: request.password,
+      );
+
+      final res = CreateAccountResponse(user: user);
+      return right(res);
+    } on AppwriteException catch (e) {
+      return left(e);
+    }
+  }
+
   /// авторизация по имейлу
   Future<Either<AppwriteException, CreateEmailSessionResponse>>
       createEmailSession(CreateEmailSessionRequest request) async {
