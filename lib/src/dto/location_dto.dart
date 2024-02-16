0c9aaa21882e1d2579dbe634565a3641c9cbaf33
egortabula <egor.vnl@gmail.com>
2024-02-16 19:04:59 +0300

    add demo constructor

--- lib/src/dto/location_dto.dart
+++ lib/src/dto/location_dto.dart
@@ -12,7 +12,6 @@ class LocationDto with LocationDtoMappable {
   final String? id;
   String name;
   String? description;
-
   List<String> images;
   String address;
   LocationTypeEnum? type;
@@ -48,6 +47,34 @@ class LocationDto with LocationDtoMappable {
     required this.features,
   })  : id = ID.unique(),
         rating = 0;
+
+  LocationDto.demo({
+    this.id = 'demo',
+    this.name = 'Набережная «Ривьера»',
+    this.description = 'Пока сама набережная «Ривьера» закрыта на реконструкцию, '
+        'советуем заглянуть в парк неподалеку. Просторные деревянные скамейки, '
+        'ухоженные дорожки и стильная каменная лестница извилистой формы. '
+        'Поднявшись, можно сделать несколько кадров с видом на море.',
+    this.lat = 43.587079,
+    this.lng = 39.713692,
+    this.isHide = false,
+    this.images = const [
+      '65c8be4cdfed24119a78',
+      '65c8be7c7c167344eb76',
+      '65c8be7c98fee66d38d1',
+      '65c8be7cab56d10653e5',
+      '65c8be7ccd833e43c105',
+    ],
+    this.type = LocationTypeEnum.location,
+    this.features = const [
+      LocationFeaturesEnum.park,
+      LocationFeaturesEnum.seafront
+    ],
+    this.rating = 0.0,
+    this.address = 'Ривьерский переулок, 7 ',
+  });
+
+  bool get isDemo => id == 'demo';
 }
 
 class LocationDtoHook extends MappingHook {
