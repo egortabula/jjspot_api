c7f84bc30c1e7356584a1ca475be8ca01ceb32ed
egortabula <egor.vnl@gmail.com>
2024-02-16 12:18:52 +0300

    fix locationCollectionId and locationsBucketId values

--- lib/src/consts.dart
+++ lib/src/consts.dart
@@ -8,8 +8,8 @@ const String databaseId = '654420dc8f7d6fb2e959';
 
 const String usersCollectionId = '654420e3028cb5bdca3e';
 
-const String locationsBucketId = '654420eb61ec78a7424e';
+const String locationsBucketId = '6544cab81f9ab838bb9a';
 
-const String locationsCollectionId = '6544cab81f9ab838bb9a';
+const String locationsCollectionId = '654420eb61ec78a7424e';
 
 const String usersBucketId = '6544caec7d776bf1fa0e';
