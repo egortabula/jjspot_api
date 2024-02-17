6671f683a6d44edfe66c88d06222124ba64e8166
egortabula <egor.vnl@gmail.com>
2024-02-17 12:48:53 +0300

    fix id param

--- lib/src/dto/user_dto.dart
+++ lib/src/dto/user_dto.dart
@@ -60,11 +60,10 @@ class UserDtoHook extends MappingHook {
     List<dynamic> locations = value['locations'];
     debugPrint('locations afterEncode: ${locations.toString()}');
 
-    reviews = reviews.map((review) => review[r'\$id']).toList();
-    locations = locations.map((location) => location[r'\$id']).toList();
+    reviews = reviews.map((review) => review['\$id']).toList();
+    locations = locations.map((location) => location['\$id']).toList();
     value['reviews'] = reviews;
     value['locations'] = locations;
-    
     return super.afterEncode(value);
   }
 }
