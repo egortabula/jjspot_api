05f20e6027f50108e93c3523505c82575301ebc7
egortabula <egor.vnl@gmail.com>
2024-02-18 00:22:10 +0300

    init

---
+++ lib/src/enums/promocode_duration_enum.dart
@@ -0,0 +1,33 @@
+import 'package:dart_mappable/dart_mappable.dart';
+
+@MappableEnum(defaultValue: PromocodeDurationEnum.undefined)
+enum PromocodeDurationEnum {
+  @MappableValue('1-день')
+  daily,
+
+  @MappableValue('3-дня')
+  threeDays,
+
+  @MappableValue('7-дней')
+  sevenDays,
+
+  @MappableValue('14-дней')
+  fourteenDays,
+
+  @MappableValue('21-день')
+  twentyOneDays,
+
+  @MappableValue('1-месяц')
+  monthly,
+
+  @MappableValue('3-месяца')
+  threeMonth,
+
+  @MappableValue('6-месяцев')
+  sixMonth,
+
+  @MappableValue('1-год')
+  yearly,
+
+  undefined,
+}
